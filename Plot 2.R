ff67fa0d9ffd90b1cbbcc97b1c74ac6847f8d061
Lance Miley <lawrence.miley@yahoo.com>
2022-04-24 10:27:18 -0400

    Added code to create the dataset_truncated table within each .R program instead of only creating in the Plot 1.R program.

--- Plot 2.R
+++ Plot 2.R
@@ -3,6 +3,10 @@ table<-read.table("household_power_consumption.txt", header=TRUE, sep=";")
 
 library(dplyr)
 
+table<-mutate(table, Date=as.Date(Date, format = "%d/%m/%Y"))
+table<-mutate(table, Global_active_power = as.numeric(Global_active_power))
+
+dataset_truncated<-filter(table, Date == "2007-02-01" | Date == "2007-02-02")
 
 date_time<-paste(dataset_truncated$Date, dataset_truncated$Time)
 date_time<-strptime(date_time, "%Y-%m-%d %H:%M:%S")
